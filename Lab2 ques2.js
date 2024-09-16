56805ccc3ec9fc8b9b19c75a502eafea75d77fcf
Abhishek-kam <156095156+Abhishek-kam@users.noreply.github.com>
2024-09-15 22:40:36 -0400

    Delete Lab2 ques2.js

--- Lab2 ques2.js
+++
@@ -1,8 +0,0 @@
-const capitalize = (str) => {
-    const [first, ...rest] = str;
-    return `${first.toUpperCase()}${rest.join('')}`;
-};
-
-
-console.log(capitalize('fooBar'));
-console.log(capitalize('nodeJs'));
